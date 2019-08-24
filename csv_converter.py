c3767dc8f5ec7b6cf92d5f3b96c5bd08fc9b011d
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2019-08-24 23:18:17 +0100

    Refactor exisiting logic, implement funlink feature

--- csv_converter.py
+++ csv_converter.py
@@ -2,13 +2,15 @@ import csv
 import io
 import json
 import logging
-
+import importlib
 
 class CSVConverter:
     config: dict
 
     def __init__(self, config_json: str = None, config_file_name: str = None, config_dict: dict = None):
         self.config = dict(config_dict) if config_dict else json.loads(config_json) if config_json else None
+        self.previous_input_rows = []
+        self.current_output_rows = []
         if not self.config:
             with open(config_file_name) as json_file:
                 self.config = json.load(json_file)
@@ -21,37 +23,43 @@ class CSVConverter:
             del(self.config['$output_config$'])
         logging.debug(f"Using config: {self.config}")
 
-    def _process_complex_column(self, line: csv.OrderedDict, item: dict):
+    def _process_func_link(self, new_heading: str, line: csv.OrderedDict, mod_func_str: str):
+        module_name, func_name = mod_func_str.split('/')
+        module = importlib.import_module(module_name)
+        func = getattr(module, func_name)
+        return func(new_heading, line, self.previous_input_rows, self.current_output_rows)
+
+    def _process_complex_column(self, column_heading: str, line: csv.OrderedDict, item: dict):
         logging.basicConfig(level=logging.DEBUG)
-        built_in_processors = {
-            "old_column": lambda a, b: a.get(b.get('old_column')) or None,
-            "default, old_column": lambda a, b: a.get(b.get('old_column')) or b.get('default'),
-            "default, lambda, old_column": None,
-            "default, lambda": None,
-            "lambda": None,
-            "lambda, old_column": None,
-        }
-        keys = list(item.keys())
-        keys.sort()
-        process_type = ', '.join(keys)
-        logging.debug(f"Process type: {process_type}")
-        if process_type in built_in_processors:
-            if "lambda" in item.keys():
-                try:
-                    exec(f"c = {item.get('lambda')}", globals())
-                    return c(line, item)
-                except:
-                    process_type = "default, old_column" if "default" in process_type else "old_column"
-            return built_in_processors[process_type](line, item)
-        pass
+
+        # the dict can have have these keys: old_column, default, lambda, funlink
+        item_output = None
+        if 'old_column' in item:
+            item_output = line.get(item.get('old_column')) or None
+
+        if 'funlink' in item:
+            mod_func_string = item.get('funlink')
+            item_output = self._process_func_link(column_heading, line, mod_func_string)
+        elif 'lambda' in item:
+            try:
+                exec(f"c = {item.get('lambda')}", globals())
+                item_output = c(line, item)
+            except:
+                pass
+        
+        if item_output is None:
+            item_output = item.get('default')
+
+        return item_output
 
     def _convert_line(self, line):
         output_line = csv.OrderedDict()
-        for new_heading, item in self.config.items():
-            if isinstance(item, str):
-                item_output = line.get(item)
+        for new_heading, options in self.config.items():
+            if isinstance(options, str):
+                item_output = line.get(options)
             else:
-                item_output = self._process_complex_column(line, item)
+                column_heading = options.get('old_column') if 'old_column' in options else new_heading
+                item_output = self._process_complex_column(column_heading, line, options)
             output_line.update({new_heading: item_output})
         # output_line = csv.OrderedDict({'Test': 'tval', 'Bla': 'Frog'})
         return output_line
@@ -63,6 +71,8 @@ class CSVConverter:
             for line in csv_data:
                 output_line = self._convert_line(line)
                 output_rows.append(output_line)
+                self.previous_input_rows.append(line)
+                self.current_output_rows.append(output_line)
             fieldnames = []
             for header_name, _ in self.config.items():
                 fieldnames.append(header_name)
